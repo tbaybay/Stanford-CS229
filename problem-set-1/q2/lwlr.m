32565e3c96055165a5c0053de6f63f3da3fa5f98
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-04-01 15:53:37 +0100

    Working implementation

--- problem-set-1/q2/lwlr.m
+++ problem-set-1/q2/lwlr.m
@@ -1,4 +1,4 @@
-function y = lwlr(X_train, y_train, x, tau)
+function y = lwlr(X_train, y_train, x)
 % Fits a logistic regression model to training data, then makes predictions
 % on x. Tau is (?)
 
@@ -10,37 +10,37 @@ function y = lwlr(X_train, y_train, x, tau)
 % resulting from your fit? (Remember to include the intercept term.)
 
 theta = zeros(2, 1);
-convergence_tolerance = 1e-2;
-error = 10;
+convergence_tolerance = 10e-20*ones(2, 1);
+del_J = [1; 1];
 i = 0;
 
-while error > convergence_tolerance
-    error = cost_gradient(X_train, y_train, theta);
-    theta = theta -  cost_hessian(X_train, y_train)\error;
-    plot(i, theta(1), 'ro');
-    hold on
-    plot(i, theta(2), 'bo');
+while abs(del_J) > convergence_tolerance
+    plot(i, theta(1), 'r.'); hold on; plot(i, theta(2), 'b.');
+    del_J = cost_gradient(X_train, y_train, theta);
+    theta = theta + 0.1*inv(cost_hessian(X_train, y_train, theta))*del_J;
+    i = i + 1;
 end
 
 end
 
-function H = cost_hessian(X_train, y_train)
-    m = size(X_train, 1); % # samples
-    n = size(X_train, 2); % # features
+function H = cost_hessian(X_train, y_train, theta)
+    m = size(X_train, 2); % # samples
+    n = size(X_train, 1); % # features
+    H = zeros(n, n);
     for i = 1:m
-        exp_term = exp(y_train(i).*theta'*X_train(i, :));
-        sample_H = (y_train(i).^2).*X_train(i, :)*X_train(i, :)'.*exp_term./((1 + exp_term).^2);
+        exp_term = exp(y_train(i).*theta'*X_train(:, i));
+        sample_H = (y_train(i).^2).*X_train(:, i)*X_train(:, i)'.*exp_term./((1 + exp_term).^2);
         H = H + sample_H;
     end
     H = H./m;
 end
 
 function del_J = cost_gradient(X_train, y_train, theta)
-    m = size(X_train, 1); % # samples
-    n = size(X_train, 2); % # features
+    m = size(X_train, 2); % # samples
+    n = size(X_train, 1); % # features
     del_J = zeros(n, 1);
     for i = 1:m
-        sample_del = y_train(i).*X_train(i, :)./(1 + exp(y_train(i).*theta'*X_train(i, :)));
+        sample_del = y_train(i).*X_train(:, i)./(1 + exp(y_train(i).*theta'*X_train(:, i)));
         del_J = del_J + sample_del;
     end
     del_J = del_J./m;
