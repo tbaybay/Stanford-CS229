71b00a17f06e7f246743186d94a6bd1d2245d3d8
Jerome Wynne <J.J.Wynne47@gmail.com>
2017-04-30 08:03:08 +0100

    Refactored nb_test into a function, nb_train into a script against various training set sizes. Included plot of test error rate against training set size.

--- problem-set-2/q3/spam_data/nb_test.m
+++ problem-set-2/q3/spam_data/nb_test.m
@@ -1,47 +1,43 @@
+function error = nb_test(phi_y, phi_spam, phi_nonspam)
+    [spmatrix, tokenlist, category] = readMatrix('MATRIX.TEST');
 
-[spmatrix, tokenlist, category] = readMatrix('MATRIX.TEST');
+    testMatrix = full(spmatrix);
+    numTestDocs = size(testMatrix, 1);
+    numTokens = size(testMatrix, 2);
 
-testMatrix = full(spmatrix);
-numTestDocs = size(testMatrix, 1);
-numTokens = size(testMatrix, 2);
+    % Assume nb_train.m has just been executed, and all the parameters computed/needed
+    % by your classifier are in memory through that execution. You can also assume 
+    % that the columns in the test set are arranged in exactly the same way as for the
+    % training set (i.e., the j-th column represents the same token in the test data 
+    % matrix as in the original training data matrix).
 
-% Assume nb_train.m has just been executed, and all the parameters computed/needed
-% by your classifier are in memory through that execution. You can also assume 
-% that the columns in the test set are arranged in exactly the same way as for the
-% training set (i.e., the j-th column represents the same token in the test data 
-% matrix as in the original training data matrix).
+    % Write code below to classify each document in the test set (ie, each row
+    % in the current document word matrix) as 1 for SPAM and 0 for NON-SPAM.
 
-% Write code below to classify each document in the test set (ie, each row
-% in the current document word matrix) as 1 for SPAM and 0 for NON-SPAM.
+    % Construct the (numTestDocs x 1) vector 'output' such that the i-th entry 
+    % of this vector is the predicted class (1/0) for the i-th  email (i-th row 
+    % in testMatrix) in the test set.
 
-% Construct the (numTestDocs x 1) vector 'output' such that the i-th entry 
-% of this vector is the predicted class (1/0) for the i-th  email (i-th row 
-% in testMatrix) in the test set.
-output = zeros(numTestDocs, 1);
-p_spam = zeros(numTestDocs, 1);
-% Used logarithms to avoid problems with underflow (although it looked like
-% Matlab was handling this fine?)
-for j = 1:numTestDocs
-    % Get the tokens that appear
-    tk_in_msg = find(testMatrix(j, :) > 0);
-    tk_nin_msg = find(testMatrix(j, :) == 0);
-    px_g_spam = exp(sum(log(phi_spam(tk_in_msg))) + sum(log(1 - phi_spam(tk_nin_msg))) + log(phi_y));
-    px_g_nonspam = exp(sum(log(phi_nonspam(tk_in_msg))) + sum(log(1 - phi_spam(tk_in_msg))) + log(1 - phi_y));
-    p_spam(j) = px_g_spam./(px_g_spam + px_g_nonspam);
-    output(j) = (p_spam(j) > 0.5);
+    % Used logarithms to avoid problems with underflow (although it looked like
+    % Matlab was handling this fine?)
+
+    output = zeros(numTestDocs, 1);
+    p_spam = zeros(numTestDocs, 1);
+
+    for j = 1:numTestDocs
+        tk_in_msg = (testMatrix(j, :) > 0);
+        tk_nin_msg = (testMatrix(j, :) == 0);
+        px_g_spam = exp(sum(log(phi_spam(tk_in_msg))) + sum(log(1 - phi_spam(tk_nin_msg))) + log(phi_y));
+        px_g_nonspam = exp(sum(log(phi_nonspam(tk_in_msg))) + sum(log(1 - phi_spam(tk_in_msg))) + log(1 - phi_y));
+        p_spam(j) = px_g_spam./(px_g_spam + px_g_nonspam);
+        output(j) = (p_spam(j) > 0.5);
+    end
+
+    % Compute the error on the test set
+    y = full(category);
+    y = y(:);
+    error = sum(y ~= output) / numTestDocs;
+
+    %Print out the classification error on the test set
+    fprintf(1, 'Test error: %1.4f\n', error);
 end
-
-%---------------
-%---------------
-
-
-% Compute the error on the test set
-y = full(category);
-y = y(:);
-error = sum(y ~= output) / numTestDocs;
-
-%Print out the classification error on the test set
-fprintf(1, 'Test error: %1.4f\n', error);
-
-
-
--- problem-set-2/q3/spam_data/nb_train.m
+++ problem-set-2/q3/spam_data/nb_train.m
@@ -1,58 +1,64 @@
+training_set_sizes = [50, 100, 200, 400, 800, 1400];
+test_errors = zeros(length(training_set_sizes), 1);
+for i = 1:length(training_set_sizes)
+    [spmatrix, tokenlist, trainCategory] = readMatrix(['MATRIX.TRAIN.'...
+                                             num2str(training_set_sizes(i))]);
 
-[spmatrix, tokenlist, trainCategory] = readMatrix('MATRIX.TRAIN');
+    trainMatrix = full(spmatrix);
+    numTrainDocs = size(trainMatrix, 1);
+    numTokens = size(trainMatrix, 2);
 
-trainMatrix = full(spmatrix);
-numTrainDocs = size(trainMatrix, 1);
-numTokens = size(trainMatrix, 2);
+    % trainMatrix is now a (numTrainDocs x numTokens) matrix.
+    % Each row represents a unique document (email).
+    % The j-th column of the row $i$ represents the number of times the j-th
+    % token appeared in email $i$. 
 
-% trainMatrix is now a (numTrainDocs x numTokens) matrix.
-% Each row represents a unique document (email).
-% The j-th column of the row $i$ represents the number of times the j-th
-% token appeared in email $i$. 
+    % tokenlist is a long string containing the list of all tokens (words).
+    % These tokens are easily known by position in the file TOKENS_LIST
 
-% tokenlist is a long string containing the list of all tokens (words).
-% These tokens are easily known by position in the file TOKENS_LIST
+    % trainCategory is a (1 x numTrainDocs) vector containing the true 
+    % classifications for the documents just read in. The i-th entry gives the 
+    % correct class for the i-th email (which corresponds to the i-th row in 
+    % the document word matrix).
 
-% trainCategory is a (1 x numTrainDocs) vector containing the true 
-% classifications for the documents just read in. The i-th entry gives the 
-% correct class for the i-th email (which corresponds to the i-th row in 
-% the document word matrix).
-
-% Spam documents are indicated as class 1, and non-spam as class 0.
-% Note that for the SVM, you would want to convert these to +1 and -1.
+    % Spam documents are indicated as class 1, and non-spam as class 0.
+    % Note that for the SVM, you would want to convert these to +1 and -1.
 
 
-%% SOLUTION %%
+    %% SOLUTION %%
 
-% What are the parameters we're fitting?
-% Two parameters for each token:
-%   > The probability of that token given y =1
-%   > The probability of that token given y = 0
-% And phi_y, the base rate : p(y = 1)
+    % What are the parameters we're fitting?
+    % Two parameters for each token:
+    %   > The probability of that token given y =1
+    %   > The probability of that token given y = 0
+    % And phi_y, the base rate : p(y = 1)
 
-% How do we fit them?
-% By calculating the fraction of occurences 
+    % How do we fit them?
+    % By calculating the fraction of occurences 
 
-% How do we use these parameters to make a prediction?
-% We use Bayes' theorem to calculate the probability of each class, given
-% the query feature vector, then select which class is more probable.
+    % How do we use these parameters to make a prediction?
+    % We use Bayes' theorem to calculate the probability of each class, given
+    % the query feature vector, then select which class is more probable.
 
-% Right, so we ned to first calculate phi_y
-%       -> Marginal probability that an email is spam
-trainCategory = full(trainCategory);
-phi_y = sum(trainCategory)/length(trainCategory);
+    % Right, so we ned to first calculate phi_y
+    %       -> Marginal probability that an email is spam
+    trainCategory = full(trainCategory);
+    phi_y = sum(trainCategory)/length(trainCategory);
 
-% Now we get the sampling distribution for each token: we'll start with the
-% spam.
-% We're calculating the probability of a word, conditional on whether it's
-% spam or not.
-phi_spam = zeros(numTokens, 1);
-phi_nonspam = zeros(numTokens, 1);
-for i = 1:numTokens
-    phi_spam(i) = (sum((trainMatrix(:, i) > 0).*(trainCategory(:) == 1)) + 1)./(sum(trainCategory == 1) + 2);
-    phi_nonspam(i) = (sum((trainMatrix(:, i) > 0).*(trainCategory(:) == 0)) + 1)./(sum(trainCategory == 0) + 2);
+    % Now we get the sampling distribution for each token: we'll start with the
+    % spam.
+    % We're calculating the probability of a word, conditional on whether it's
+    % spam or not.
+    phi_spam = zeros(numTokens, 1);
+    phi_nonspam = zeros(numTokens, 1);
+    for j = 1:numTokens
+        phi_spam(j) = (sum((trainMatrix(:, j) > 0).*(trainCategory(:) == 1)) + 1)./(sum(trainCategory == 1) + 2);
+        phi_nonspam(j) = (sum((trainMatrix(:, j) > 0).*(trainCategory(:) == 0)) + 1)./(sum(trainCategory == 0) + 2);
+    end
+    
+    test_errors(i) = nb_test(phi_y, phi_spam, phi_nonspam);
+
+    % Note that the + 1 and +2s implement Laplace smoothing - in the absence of
+    % any previous instances of a token, we want the classifier to assign equal
+    % weights to spam/non-spam.
 end
-
-% Note that the + 1 and +2s implement Laplace smoothing - in the absence of
-% any previous instances of a token, we want the classifier to assign equal
-% weights to spam/non-spam.
